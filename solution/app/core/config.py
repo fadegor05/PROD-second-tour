c9336cc05f734533f23e47d3a19246a8bc4e2850
fadegor05 <fad.egor05@gmail.com>
2024-02-27 20:57:02 +0400

    fixed db url problem

--- solution/app/core/config.py
+++ solution/app/core/config.py
@@ -1,3 +1,7 @@
 import os
+from sqlalchemy.engine import make_url
+from dotenv import load_dotenv, find_dotenv
 
-SQLALCHEMY_DB_URL = os.environ.get('POSTGRES_CONN')
+load_dotenv(find_dotenv())
+
+SQLALCHEMY_DB_URL = make_url(os.environ.get('POSTGRES_CONN').replace('postgres://', 'postgresql://'))
